bd4e86bb946594fb4665a865bd264c7d9e29c752
Sting Chang <stingchang@users.noreply.github.com>
2016-07-10 22:51:11 -0700

    Create Q319_BuildSwitcher.java

---
+++ java/Q319_BuildSwitcher.java
@@ -0,0 +1,24 @@
+/**
+ * @author stingchang
+ * */
+
+package com.leet.q311;
+
+public class Q319_BuildSwitcher {
+
+	public int bulbSwitch(int n) {
+		return (int) (Math.sqrt(n));
+	}
+
+	// every bulb on then off except perfect square
+	// ex: 7
+	// 0000000
+	// 1111111
+	// 1010101
+	// 1000111
+	// 1001111
+	// 1001011
+	// 1001001
+	// 1001000
+
+}
